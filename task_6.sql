84c9af75be5a2b1e8f28aa02943653d224a6f802
Lelo Mohamed <lelomohammedpg@gmail.com>
2024-08-12 15:06:35 +0300

    SQL query to insert multiple row value in table called customers

--- task_6.sql
+++ task_6.sql
@@ -3,6 +3,6 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave'),
-	   (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 HAppiness Ave'),
-	   (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave');
+VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', "124 Happiness Ave."),
+	   (3, 'Obed Ehoneah', 'eobed@sandtech.com', "125 HAppiness Ave."),
+	   (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', "126 Happiness Ave.");
